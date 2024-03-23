adca768cd1841eb104c6e73a144062fa44716bb0
mikeqiu <mikeqiu2011@163.com>
2024-03-23 10:34:55 +0800

    @AfterReturning

--- src/main/java/main/LoggingAspect.java
+++ src/main/java/main/LoggingAspect.java
@@ -1,6 +1,7 @@
 package main;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -13,21 +14,9 @@ import java.util.logging.Logger;
 public class LoggingAspect {
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
-    @Around("@annotation(ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        var methodName = joinPoint.getSignature().getName();
-        var arguments = joinPoint.getArgs();
-
-        logger.info("Method " + methodName +
-                " with parameters " + Arrays.asList(arguments) +
-                " will execute");
-
-//        var comment = new Comment("Some other text", "another author");
-//        Object[] newArgs = {comment};
-
-        var returnedByMethod = joinPoint.proceed();
-        logger.info("method executed and returned " + returnedByMethod);
-
-        return returnedByMethod;
+    @AfterReturning(value = "@annotation(ToLog)",
+            returning = "returnedValue")
+    public void log(Object returnedValue) throws Throwable {
+        System.out.println("method executed and returned " + returnedValue);
     }
 }
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -11,12 +11,13 @@ public class CommentService {
     private Logger logger =
             Logger.getLogger(CommentService.class.getName());
 
+    @ToLog
     public String publishComment(Comment comment) {
         logger.info("Publishing comment:" + comment.text());
         return "SUCCESS";
     }
 
-    @ToLog
+//    @ToLog
     public void deleteComment(Comment comment){
         logger.info("Deleting comment: " + comment.text());
     }
